cbc38cef2745445da8046f3e4e0b56db6cb9859f
Subrat vats <subratkumar321@gmail.com>
2024-06-26 15:31:36 +0530

    first commit changes

---
+++ Tuturials/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "C:\\Users\\subra\\Desktop\\terraform\\Tuturials\\file1.txt"
+}
+variable "content" {
+default = "We love pets!"
+}
+variable "prefix" {
+default = "Mrs"
+}
+variable "separator" {
+default = "."
+}
+variable "length" {
+default = "1"
+}
